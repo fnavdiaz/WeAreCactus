7cde25742d7e05d4ec04917591ff36055c0c39fc
fnavdiaz <fnavdiaz@gmail.com>
2025-07-22 17:46:43 +0200

    Put correct names

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreAppConfig(AppConfig):
+class AppWACAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core_app'
+    name = 'core'
